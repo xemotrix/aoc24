1c363818fd6a7ef4e54de5728aa675ccbadb5111
chema.rodriguez <chema.rodriguez@cabify.com>
2024-12-01 21:20:09 +0100

    avoid .:

--- hs/app/Day1.hs
+++ hs/app/Day1.hs
@@ -1,15 +1,15 @@
 module Day1 (run) where
 
-import Combinators
+import Combinators (both)
 import Control.Arrow ((&&&))
 import Data.List (sort, transpose)
-import Utils
+import Utils (chunk, combinations)
 
 run :: String -> IO (String, String)
 run = return . both show . (part1 &&& part2) . parse
 
 part1, part2 :: ([Int], [Int]) -> Int
-part1 = sum . uncurry (zipWith (abs .: (-))) . both sort
+part1 = sum . map abs . uncurry (zipWith (-)) . both sort
 part2 = sum . map fst . filter (uncurry (==)) . uncurry combinations
 
 parse :: String -> ([Int], [Int])
