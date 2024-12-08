b4c30387911c5742b63525a5db10e8d9bb0af66c
chema.rodriguez <chema.rodriguez@cabify.com>
2024-12-08 18:09:38 +0100

    day 8

--- hs/app/Day8.hs
+++ hs/app/Day8.hs
@@ -1,36 +1,38 @@
 module Day8 (run) where
 
-import Combinators (both, (.:))
+import Combinators (both, ($$), (.:))
 import Control.Arrow (first, (&&&), (***))
 import Data.Function (on)
 import Data.List (groupBy, nub, sortBy)
 import Utils (combinations, indexMat)
 
 run :: String -> (String, String)
-run = both show . (part1 &&& part2) . parse
+run = both show . (run' part1 &&& run' part2) . parse
+  where
+    run' = uncurry score .: first
 
 type Ant = (Int, Int)
 
-part1, part2 :: ((Int, Int), [[Ant]]) -> Int
-part1 = uncurry score . first (take 1 . drop 1 .: takeWhile . inBounds)
-part2 = uncurry score . first (takeWhile . inBounds)
+part1, part2 :: (Int, Int) -> [Ant] -> [Ant]
+part1 = take 1 . drop 1 .: part2
+part2 = takeWhile . inBounds
 
 inBounds :: (Int, Int) -> Ant -> Bool
 inBounds (h, w) (y, x) = y >= 0 && y < h && x >= 0 && x < w
 
 score :: ([Ant] -> [Ant]) -> [[Ant]] -> Int
-score nFilter ants = length $ nub $ concatMap (genAntinodes nFilter) ants
+score f = length . nub . concatMap (genAntinodes f)
 
 genAntinodes :: ([Ant] -> [Ant]) -> [Ant] -> [Ant]
-genAntinodes nFilter ants =
-  nub $ concatMap (antinodes nFilter) (combinations ants ants)
+genAntinodes f = nub . concatMap (antinodes f) . combis
+  where
+    combis = filter (uncurry (/=)) . (combinations $$)
 
 antinodes :: ([Ant] -> [Ant]) -> (Ant, Ant) -> [Ant]
-antinodes _ (a, b) | a == b = []
-antinodes nFilter ants = harmonics (+) ++ harmonics (-)
+antinodes f = (++) <$> harmonics (+) <*> harmonics (-)
   where
-    harmonics op = nFilter $ (iterate <$> harmonicFun op <*> fst) ants
-    harmonicFun op ((y, x), (y', x')) = op (y - y') *** op (x - x')
+    harmonics op = f . (iterate <$> nextHarmonic op <*> fst)
+    nextHarmonic op ((y, x), (y', x')) = op (y - y') *** op (x - x')
 
 parse :: String -> ((Int, Int), [[Ant]])
 parse = (parseSize &&& parseAnts) . lines
