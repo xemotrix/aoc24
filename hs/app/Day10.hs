1e61737ba2f3dca1e3eac3ef7216a295e73bacbf
chema.rodriguez <chema.rodriguez@cabify.com>
2024-12-10 11:29:05 +0100

    day 10

--- hs/app/Day10.hs
+++ hs/app/Day10.hs
@@ -1,7 +1,7 @@
 module Day10 (run) where
 
-import Combinators (both)
-import Control.Arrow (second, (&&&))
+import Combinators (both, (.:))
+import Control.Arrow (first, second, (&&&))
 import Data.List (nub, singleton)
 import Data.Map (Map)
 import Data.Map qualified as M
@@ -13,27 +13,25 @@ run = both show . (part1 &&& part2) . parse
 
 type Pos = ((Int, Int), Int)
 
-type NeigFun = Pos -> [Pos]
+part1, part2 :: (Pos -> [Pos], [Pos]) -> Int
+part1 = sum . uncurry map . first (length . nub .: calcNines)
+part2 = sum . uncurry map . first (length .: calcNines)
 
-part1, part2 :: ([Pos], NeigFun) -> Int
-part1 (zeros, neigf) = sum $ map (length . nub . calcNines neigf) zeros
-part2 (zeros, neigf) = sum $ map (length . calcNines neigf) zeros
-
-calcNines :: NeigFun -> Pos -> [Pos]
+calcNines :: (Pos -> [Pos]) -> Pos -> [Pos]
 calcNines neigf = ((++) <$> getNines <*> getRest) . neigf
   where
-    getNines = nub . filter ((== 9) . snd)
+    getNines = nub . filter ((9 ==) . snd)
     getRest = concatMap (calcNines neigf) . filter ((/= 9) . snd)
-
-parse :: String -> ([Pos], Pos -> [Pos])
-parse = (parseZeros &&& parseNeigF) . toMat
-  where
-    toMat = map (second (read . singleton)) . indexMat . lines
-    parseZeros = filter ((== 0) . snd)
-    parseNeigF = neigs . M.fromList . map (fst &&& id)
 
 neigs :: Map (Int, Int) Pos -> Pos -> [Pos]
 neigs m ((y, x), height) = filter diff1 $ mapMaybe (`M.lookup` m) coords
   where
-    diff1 = (== 1) . subtract height . snd
+    diff1 = (height + 1 ==) . snd
     coords = [(y - 1, x), (y + 1, x), (y, x - 1), (y, x + 1)]
+
+parse :: String -> (Pos -> [Pos], [Pos])
+parse = (parseNeigF &&& parseZeros) . toMat
+  where
+    toMat = map (second (read . singleton)) . indexMat . lines
+    parseZeros = filter ((0 ==) . snd)
+    parseNeigF = neigs . M.fromList . map (fst &&& id)
